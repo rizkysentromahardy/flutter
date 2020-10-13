95d6e0ee4de6d572afa7abc17a813e93c91d93f6
rizkysentromahardy <rizkysentro3@gmail.com>
2020-10-13 20:42:46 +0700

    belajar stateless dan statefull

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,39 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+  void tekantombol() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Container"),
+          title: Text("belajar statfull dan stateless"),
         ),
-        body: Container(
-          color: Colors.red[200],
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(10),
-          child: Container(
-            // margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                gradient: LinearGradient(
-                    begin: Alignment.center,
-                    end: Alignment.topCenter,
-                    colors: <Color>[
-                      Colors.amber[100],
-                      Colors.greenAccent[200]
-                    ])),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                number.toString(),
+                style: TextStyle(fontSize: 10 + number.toDouble()),
+              ),
+              RaisedButton(
+                  child: Text("tambah bilangan"), onPressed: tekantombol)
+            ],
           ),
         ),
       ),
